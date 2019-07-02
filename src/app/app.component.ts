e97ae48ce82c0897fb14109e62d5cb8dec4f9ae8
Janice-M <janiceink001@gmail.com>
2019-07-02 10:45:54 +0300

    importing componebt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 
 import { Component } from '@angular/core';
-
+import { Goal } from './goal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals:string[];
-
-  constructor(){
-    this.goals = ['Drink some redbull', 'Compile some code', 'Chat with the buddies']
-  }
+  goals:Goal = [
+    {id:1, name:'Drink redbull'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 }
